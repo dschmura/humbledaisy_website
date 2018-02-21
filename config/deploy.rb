29f46a8f758f7fdf5b41995b3bd86f625f1df070
David Chmura <dschmura@humbledaisy.com>
2018-02-21 05:44:29 -0500

    FIX: Comment flag replaced.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@
   set :puma_threads,    [4, 16]
   set :puma_workers,    0
 
-  Don't change these unless you know what you're doin
+  # Don't change these unless you know what you're doin
   set :pty,             true
   set :use_sudo,        false
   set :stage,           :production
